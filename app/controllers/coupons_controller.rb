1a608f8c79d0bcfd9b9c7f1ed61668fcf12c6323
Alyssa Kim <alyssa.kim@gmail.com>
2018-06-29 13:12:02 +0000

    automatically backed up by learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,2 +1,19 @@
 class CouponsController < ApplicationController
-end 
+  def index
+    @coupons = Coupon.all
+  end
+
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @coupon = Coupon.new
+    @coupon.coupon_code = params[:coupon_code]
+    @coupon.store = params[:store]
+  end
+
+end
