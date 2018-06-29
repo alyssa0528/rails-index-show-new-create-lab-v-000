7ab01a211855b11379a13ffb98307f77724b3646
Alyssa Kim <alyssa.kim@gmail.com>
2018-06-29 13:11:57 +0000

    automatically backed up by learn

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,19 +1,3 @@
 class Coupon < ActiveRecord::Base
-  def index
-    @coupons = Coupon.all
-  end
-
-  def show
-    @coupon = Coupon.find(params[:id])
-  end
-
-  def new
-  end
-
-  def create
-    @coupon = Coupon.new
-    @coupon.coupon_code = params[:coupon_code]
-    @coupon.store = params[:store]
-  end
-
+  
 end
